c3fb4885d822493bdb561ca6e16cf3b07aafd786
Surton Redjopawiro <106982636+surton-train@users.noreply.github.com>
2022-06-06 17:50:07 +0200

    Update streamlit_app.py
    fix header and adding  text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard boiled Free-Range Egg')
+
