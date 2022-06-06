a0e7739963551fb29e1d667693e487eb43e9c694
Surton Redjopawiro <106982636+surton-train@users.noreply.github.com>
2022-06-06 20:29:36 +0200

    Update streamlit_app.py
    choose the fruit name column as the index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+# set my_fruit_list as a list: choose the fruit name column as the index
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # lets put a pick list se they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
