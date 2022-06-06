7b98e16cd73f7884d2cea82c4bb7a3829c298a87
Surton Redjopawiro <106982636+surton-train@users.noreply.github.com>
2022-06-06 21:04:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,24 @@
 import streamlit
-
 streamlit.title('My parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🥑🍞 Build Your Own Fruit Smoothie 🥣 ')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 # show my fruit list
 #streamlit.dataframe(my_fruit_list)
-
 # set my_fruit_list as a list: choose the fruit name column as the index
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # lets put a pick list se they can pick the fruit they want to include
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits:" , list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+#streamlit.multiselect("Pick some fruits:" , list(my_fruit_list.index),['Avocado','Strawberries'])
 
+streamlit.dataframe(my_fruit_list)
 ## show all daat form the selected list
 fruits_selected = streamlit.multiselect("Pick some fruits:" , list(my_fruit_list.index),['Avocado','Strawberries'])
 #fruit_to_show = my_fruit_list.loc[fruits_selected]
