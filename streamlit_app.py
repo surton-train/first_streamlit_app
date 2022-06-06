b3894e5dc2504835c030ad42c82330eb9e555995
Surton Redjopawiro <106982636+surton-train@users.noreply.github.com>
2022-06-06 20:34:54 +0200

    Update streamlit_app.py
    Adding new header Build your own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🥑🍞 Build Your Own Fruit Smoothie 🥣 ')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # show my fruit list
